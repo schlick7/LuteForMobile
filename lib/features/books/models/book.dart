cc400d45a7fc9f983f671cc5261d5781b45d6072
Cody <codyschlickbernd@gmail.com>
2025-12-29 22:20:30 -0600

    more fix attempts

--- lib/features/books/models/book.dart
+++ lib/features/books/models/book.dart
@@ -9,7 +9,7 @@ class Book {
   final int currentPage;
   final int percent;
   final int wordCount;
-  final int? distinctTerms;
+  final int distinctTerms;
   final double unknownPct;
   final List<int> statusDistribution;
 
@@ -22,25 +22,30 @@ class Book {
     required this.currentPage,
     required this.percent,
     required this.wordCount,
-    this.distinctTerms,
+    required this.distinctTerms,
     required this.unknownPct,
     required this.statusDistribution,
   });
 
   factory Book.fromJson(Map<String, dynamic> json) {
+    final isCompleted = json['IsCompleted'];
+    final distinctCount = json['DistinctCount'];
+    final unknownPercent = json['UnknownPercent'];
+    final statusDist = json['StatusDistribution'];
+
     return Book(
       id: json['BkID'] as int,
       title: json['BkTitle'] as String,
       language: json['LgName'] as String,
-      langId: json['LgID'] as int? ?? 0,
+      langId: 0,
       totalPages: json['PageCount'] as int,
       currentPage: json['PageNum'] as int,
-      percent: ((json['IsCompleted'] as int? ?? 0) * 100),
+      percent: ((isCompleted is int ? isCompleted : 0) * 100),
       wordCount: json['WordCount'] as int,
-      distinctTerms: json['DistinctCount'] as int?,
-      unknownPct: (json['UnknownPercent'] as num?)?.toDouble() ?? 0.0,
+      distinctTerms: (distinctCount is int) ? distinctCount : 0,
+      unknownPct: (unknownPercent is num) ? unknownPercent.toDouble() : 0.0,
       statusDistribution: _parseStatusDist(
-        json['StatusDistribution'] as String? ?? '',
+        (statusDist is String) ? statusDist : '',
       ),
     );
   }
@@ -51,18 +56,26 @@ class Book {
     }
 
     try {
-      final Map<String, dynamic> parsed = json.decode(dist);
+      final Map<String, dynamic> parsed = jsonDecode(dist);
       return [
-        parsed['0'] as int? ?? 0,
-        parsed['1'] as int? ?? 0,
-        parsed['2'] as int? ?? 0,
-        parsed['3'] as int? ?? 0,
-        parsed['4'] as int? ?? 0,
-        parsed['5'] as int? ?? 0,
+        _getInt(parsed, '0'),
+        _getInt(parsed, '1'),
+        _getInt(parsed, '2'),
+        _getInt(parsed, '3'),
+        _getInt(parsed, '4'),
+        _getInt(parsed, '5'),
       ];
     } catch (e) {
       return List.generate(6, (i) => 0);
     }
+  }
+
+  static int _getInt(Map<String, dynamic> map, String key) {
+    final value = map[key];
+    if (value is int) {
+      return value;
+    }
+    return 0;
   }
 
   String get pageProgress => '$currentPage/$totalPages';
