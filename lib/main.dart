e723c56e512fd46d2703d582127e9eb3ca2e3560
Cody <codyschlickbernd@gmail.com>
2026-01-01 08:39:00 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter/services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:lute_for_mobile/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-
-  runApp(
-    ProviderScope(
-      overrides: [
-        initialServerUrlProvider.overrideWithValue(
-          prefs.getString('server_url') ?? '',
-        ),
-      ],
-      child: const App(),
-    ),
-  );
+void main() {
+  runApp(const ProviderScope(child: App()));
 }
-
-final initialServerUrlProvider = Provider<String>((ref) => '');
